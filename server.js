056f02d181a637811fefaed6fc22f4f1c20f070f
Michael <michael.lau@hotmail.ca>
2026-02-01 13:44:36 -0800

    Modify port to have env variable

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ class Server {
 
     // Start listening on port 8888
     start(){
-        this.server.listen(8888);
+        this.server.listen(getenv.PORT);
     }
 
     // Returns a greeting string with name and date
