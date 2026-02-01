ca0e066fdffe8fb6c1161e5f5ab91d1abbe27b1e
Michael <michael.lau@hotmail.ca>
2026-02-01 14:30:41 -0800

    Add base url path in en.js

--- lang/en/en.js
+++ lang/en/en.js
@@ -1,7 +1,8 @@
 const STRINGS = {
     GREETING : "Hello %1, What a beautiful day. Server current date and time is",
     ERROR: "404: File not found",
-    SUCCESS: "Successfully wrote to file.txt"
+    SUCCESS: "Successfully wrote to file.txt",
+    BASE_PATH : "/COMP4537/labs/3"
 }
 
 module.exports = STRINGS;
